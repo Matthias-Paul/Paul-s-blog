da4eecd3afd9cd3012c307a6b7bd193f23276729
Matthias-Paul <140503382+Matthias-Paul@users.noreply.github.com>
2024-11-14 05:10:17 -0800

    update

--- api/index.js
+++ api/index.js
@@ -1,11 +1,14 @@
 import express from "express"
+import mongoose from "mongoose"
 
 const app = express();
+
+mongoose.connect()
 
 
 
 app.listen(8000, ()=>{
-    console.log("app is running on port 3000!");
+    console.log("app is running on port 3000!!");
 })
 
 
