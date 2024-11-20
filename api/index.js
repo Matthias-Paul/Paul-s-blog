083e1ff96d2a9dbeffe7e72fa99c29a8215f762a
Matthias-Paul <140503382+Matthias-Paul@users.noreply.github.com>
2024-11-20 09:26:47 -0800

    update

--- api/index.js
+++ api/index.js
@@ -1,23 +1,31 @@
-import express from "express"
-import mongoose from "mongoose"
-import dotenv from "dotenv"
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js"
+import authRoutes from "./routes/auth.route.js"
 
+dotenv.config();
 
-dotenv.config()
+const PORT = process.env.PORT 
+const MONGO = process.env.MONGO
+
 const app = express();
 
-mongoose.connect(process.env.MONGO ).then (()=>{
-    console.log("MongoDB is connected")
-}).catch(err => {console.log(err)})
+// Middleware
+app.use(express.json());
+
+// Database Connection
+mongoose
+  .connect(MONGO)
+  .then(() => console.log("MongoDB is connected"))
+  .catch((err) => console.error("MongoDB connection error:", err.message));
+
+// Routes
+app.use("/api/user", userRoutes)
+app.use("/api/auth", authRoutes)
 
 
-
-
-app.listen(8000, ()=>{
-    console.log("app is running on port 3000!!");
+// Start Server 
+app.listen(PORT, () => {
+  console.log("App is running on port", PORT);
 })
-
-
-
-
-
