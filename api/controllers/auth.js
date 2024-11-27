8586fc5f10cdd4e1d01cb6da184b3aac020522d7
Matthias-Paul <140503382+Matthias-Paul@users.noreply.github.com>
2024-11-27 13:25:41 -0800

    update

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,6 +1,7 @@
 import User from "../model/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js"
+import jwt from "jsonwebtoken"
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -49,16 +50,25 @@ export const signin = async (req, res, next)=>{
   try {
     const validUser = await User.findOne({ email });
     if(!validUser ) {
-      next(errorHandler(400, "User not found"))
+     return next(errorHandler(400, "User not found"))
     }
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if(!validPassword ) {
-      next(errorHandler(400, "Invalid password"))
+      return  next(errorHandler(400, "Invalid password"))
     }
 
+    const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
 
+    const { password: pass, ...rest } = validUser._doc;
+     res.status(200).cookie("access-token", token, {
+      httpOnly:true
+     }).json(rest)
   } catch (error) {
-    next(error)
+    next(error);
   }
 
 }
+
+
+
+
