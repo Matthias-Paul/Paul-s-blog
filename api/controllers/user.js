ed92ed778617f2fd1827669567460bd8c5f838d0
Matthias-Paul <140503382+Matthias-Paul@users.noreply.github.com>
2024-12-17 00:10:51 -0800

    update

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -2,5 +2,7 @@ export const  test = (req, res)=>{
     res.send("API Call");
 }
 export const  updateUser = async (req, res, next)=>{
-     console.log(req.user)
+     console.log(req.user);
+    
+
 }
