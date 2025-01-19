958d0703051b345094ba6986634000d4aba88d45
Matthias-Paul <140503382+Matthias-Paul@users.noreply.github.com>
2025-01-19 00:26:44 -0800

    update

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ import userRoutes from "./routes/user.route.js"
 import authRoutes from "./routes/auth.route.js"
 import cors from "cors"
 import cookieParser from "cookie-parser" 
-
+import path from "path"
 
 dotenv.config();
 
@@ -27,6 +27,8 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser())
 
+
+const _dirname = path.resolve()
 // Database Connection
 mongoose  
   .connect(MONGO)
@@ -52,7 +54,10 @@ app.use((err, req, res, next)=>{
 })
 
 
+app.use(express.static(path.join(_dirname, "/client/dist")))
 
+app.get("*", (req, res)  => res.sendFile(path.join(_dirname, "/client/dist/index.html")))
+  
 
 // Start Server 
 app.listen(PORT, () => {
