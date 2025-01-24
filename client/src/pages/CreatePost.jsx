162aa7390819037f2b7e70e5b3a2440d0aff5a47
Matthias-Paul <140503382+Matthias-Paul@users.noreply.github.com>
2025-01-24 12:02:34 -0800

    update

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,11 +1,64 @@
 
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import { useState } from "react";
 
 function CreatePost() {
+
+  const [UploadImage, setUploadImage] = useState(null);
+  const [isUploading, setIsUploading] = useState(false);
+  const [uploadImageURL, setUploadImageURL] = useState("");
+  const [imageUploadErrorMessage, setImageUploadErrorMessage] = useState(null)
+
+  const handlePostImage = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setUploadImage(file);
+    }
+  };
+
+   
+      const handleUploadImage = async () => {
+        if(!UploadImage){
+          setImageUploadErrorMessage("Please select an image.")
+
+        }
+          if(UploadImage){
+            setImageUploadErrorMessage(null)
+
+            const data = new FormData();
+            data.append("file", UploadImage);
+            data.append("upload_preset", "cloudinary_backend");
+            data.append("cloud_name", "drkxtuaeg");
+      
+            try {
+              setIsUploading(true);
+              const res = await fetch("https://api.cloudinary.com/v1_1/drkxtuaeg/image/upload", {
+                method: "POST",
+                body: data,
+              });
+      
+              if (!res.ok) {
+                throw new Error("Image upload failed. Please try again.");
+              }
+      
+              const result = await res.json();
+              setUploadImageURL(result.secure_url); // Save uploaded image URL
+              console.log("Uploaded Image URL:", result.secure_url);
+            } catch (error) {
+              console.error(error.message);
+              setImageUploadErrorMessage("Image upload failed. Please try again.")
+
+            } finally {
+              setIsUploading(false);
+            }
+          }
+
+    }
+ 
   return (
     <>
-      <div className="w-full  m-auto px-[20px] lg:px-[30px] pb-[350px] " >
+      <div className={`w-full  m-auto px-[20px] lg:px-[30px] ${uploadImageURL==""? "pb-[350px]":"pb-[750px]"} `} >
         <div   className=" h-screen max-w-[1400px] m-auto pt-[70px] ">
           <h1 className="text-center font-[600] text-[24px] sm:text-[30px] my-[25px] " > Create a post </h1>
           <form className="flex flex-col m-auto max-w-[800px] " >
@@ -19,9 +72,21 @@ function CreatePost() {
                  </select>
              </div>
                <div className="flex border flex-col sm:flex-row border-[4px] border-[blue] border-dotted rounded-lg sm:my-[35px] p-[10px] ">
-               <input className="mr-[10px] w-full sm:flex-1 border-none  mb-[10px] sm:mb-[0px] rounded-lg text-white bg-[gray] "  accept="image/>*"  id="file" type="file"  />
-               <button type="button" className="bg-[blue] text-white font-[500] hover:bg-[gray] p-[10px] rounded-lg cursor-pointer " >Upload image </button>
+               <input className="mr-[10px] w-full sm:flex-1 border-none  mb-[10px] sm:mb-[0px] rounded-lg text-white bg-[gray] " onChange={handlePostImage}   accept="image/>*"  id="file" type="file"  />
+               <button type="button" className="bg-[blue] text-white font-[500] hover:bg-[gray] p-[10px] rounded-lg cursor-pointer " onClick={handleUploadImage} >  {isUploading? "Uploading... ": "Upload image" }</button>
+
                </div>
+               {
+                imageUploadErrorMessage && (
+                  <div className="text-start mt-[-30px] mb-[10px] text-red-500 "> {imageUploadErrorMessage} </div>
+                )
+               }
+               {
+                uploadImageURL && (
+                  <div className="mt-[20px] sm:mt-[0px] mb-[10px] w-full sm:mb-[20px] m-auto "> <img className=" rounded-[2px] w-full object-cover  max-h-[400px] " src={uploadImageURL} /> </div>
+                )
+               }
+              
               
                <ReactQuill theme="snow" required className=" mt-[30px] h-72 mb-[70px] sm:mt-[0px] " placeholder="Write something..." />
                
@@ -32,6 +97,6 @@ function CreatePost() {
       </div>
     </>
   )
-}
 
+}
 export default CreatePost 
