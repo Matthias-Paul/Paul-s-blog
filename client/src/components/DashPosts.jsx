c465f38173cba30f03f75afb7e6584bc15a094f8
Matthias-Paul <140503382+Matthias-Paul@users.noreply.github.com>
2025-02-01 05:54:24 -0800

    use react query to fetch data

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,113 +1,103 @@
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import { useSelector } from "react-redux";
-import { Table } from "flowbite-react"
-import { NavLink } from "react-router-dom"
-
+import { Table } from "flowbite-react";
+import { NavLink } from "react-router-dom";
 
 function DashPosts() {
+  const { currentUser } = useSelector((state) => state.user);
 
-  const { currentUser} = useSelector(state => state.user)
-   const [userPosts, setUserPosts] = useState([])
-const [showMore, setShowMore] = useState(true)
+  // Define the fetchPosts function
+  const fetchPosts = async () => {
+    const res = await fetch(
+      `https://paul-s-blog.onrender.com/api/post/get-post?userId=${currentUser.user._id}`
+    );
+    if (!res.ok) {
+      throw new Error("Failed to fetch posts");
+    }
+    return res.json();
+  };
 
-  useEffect(() => {
-       const fetchPosts= async()=>{     
-        try {
-          
-          const res = await fetch(`https://paul-s-blog.onrender.com/api/post/get-post?userId=${currentUser.user._id}`)
-          const data = await res.json()
-         console.log(data)
-         if(res.ok){
-          setUserPosts(data.posts)
-          if(data.posts.length < 9){
-            setShowMore(false)
-          }
-         }
-         
-        } catch (error) {
-          console.log(error)
-        }
-      }
-     if(currentUser.user.isAdmin){
+  // Use React Query to fetch posts
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["posts", currentUser.user._id],
+    queryFn: fetchPosts,
+    enabled: currentUser.user.isAdmin, // Only fetch if the user is an admin
+  });
 
-      fetchPosts()
+  const posts = data?.posts || [];
+  const showMore = posts.length >= 9;
 
-     }
-  }, [currentUser.user._id, currentUser.user.isAdmin]);
+  if (isLoading) {
+    return <div className=" mx-auto  text-xl md:text-2xl" >Loading posts...</div>;
+  }
+
+  if (isError) {
+    return <div className=" mx-auto text-xl md:text-2xl " >Failed to load posts. Please try again later.</div>;
+  }
+
   return (
-
     <>
-      <div className="w-full   mx-auto  pb-[50px] ">
-        <div className=" md:ml-[260px]  px-[12px] sm:px-[20px] mx-auto " >
-         {
-         currentUser.user.isAdmin && userPosts.length > 0 ? (
-          <>
-           <div className=" sm:p-[20px] rounded-[5px] md:mx-auto overflow-x-scroll  mt-[10px]  scrollbar scrollbar-thumb-[gray]  ">
-          <Table className=" w-[1027px] static mx-auto table-auto bg-transparent dark:bg-transparent " >
-           <Table.Head >
-          
-            <Table.HeadCell>  <div className=" "> Date Updated </div> </Table.HeadCell>
-            <Table.HeadCell> Post Image</Table.HeadCell>
-            <Table.HeadCell> Post Title </Table.HeadCell>
-            <Table.HeadCell> Category </Table.HeadCell>
-            <Table.HeadCell> Delete </Table.HeadCell>
-            <Table.HeadCell> <span> Edit </span> </Table.HeadCell>
-           
-           </Table.Head> 
+      <div className="w-full mx-auto pb-[50px]">
+        <div className="md:ml-[280px] px-[12px] md:px-[20px]  mx-auto">
+          {currentUser.user.isAdmin && posts.length > 0 ? (
+            <>
+              <div className="sm:p-[20px] md:mt-[-3px] rounded-[5px] md:shadow-md dark:border md:mx-auto overflow-x-scroll mt-[10px] scrollbar scrollbar-thumb-[gray]">
+                <Table className="w-[1000px] static mx-auto table-auto bg-transparent  dark:bg-transparent">
+                  <Table.Head>
+                    <Table.HeadCell>
+                      <div>Date Updated</div>
+                    </Table.HeadCell>
+                    <Table.HeadCell>Post Image</Table.HeadCell>
+                    <Table.HeadCell>Post Title</Table.HeadCell>
+                    <Table.HeadCell>Category</Table.HeadCell>
+                    <Table.HeadCell>Delete</Table.HeadCell>
+                    <Table.HeadCell>Edit</Table.HeadCell>
+                  </Table.Head>
 
-           {
-            userPosts.map((post) =>(
-                  <Table.Body  key={post._id}>
-                  <Table.Row >  
-                    <Table.Cell> <div className=""> {new Date(post.updatedAt).toLocaleDateString()}</div> </Table.Cell>
-                    <Table.Cell> 
-                      <NavLink to={`/post/${post.slug}`}>
-                        <img  src={post.image} alt={post.title} className=" my-[15px] cursor-pointer max-w-[75px] rounded-[5px] bg-[gray] " />
-                      </NavLink>          
-                       </Table.Cell>
-                       <Table.Cell> 
-                      <NavLink to={`/post/${post.slug}`}>
-                        {post.title}                      
-                      </NavLink>
-                       </Table.Cell>
-                       <Table.Cell> 
-                        {post.category}                      
-                 
-                       </Table.Cell>
-
-                       <Table.Cell> 
-                         <span className=" text-red-400 hover:underline cursor-pointer">
-                            Delete
-                         </span>                       
-                 </Table.Cell>
-                 <Table.Cell> 
-                      <NavLink to={`/edit-post/${post._id}`}>
-                        Edit                      
-                      </NavLink>
-                       </Table.Cell>
-
-                 
-                  </Table.Row>
-                  </Table.Body>  
-            ))
-           }
-          </Table>
-         
-             </div>
-             {
-            showMore && (
-              <div>
-                <button className="bg-[blue] p-[10px] rounded-lg text-white cursor-pointer rounded-lg " > Show more </button>
+                  {posts.map((post) => (
+                    <Table.Body key={post._id}>
+                      <Table.Row>
+                        <Table.Cell>
+                          <div>{new Date(post.updatedAt).toLocaleDateString()}</div>
+                        </Table.Cell>
+                        <Table.Cell>
+                          <NavLink to={`/post/${post.slug}`}>
+                            <img
+                              src={post.image}
+                              alt={post.title}
+                              className="my-[15px] cursor-pointer max-w-[75px] rounded-[5px] bg-[gray]"
+                            />
+                          </NavLink>
+                        </Table.Cell>
+                        <Table.Cell >
+                          <NavLink to={`/post/${post.slug}`}>
+                          <div className=" truncate max-w-[250px] mr-[0px] "> {post.title} </div>
+                          </NavLink>
+                        </Table.Cell>
+                        <Table.Cell>{post.category}</Table.Cell>
+                        <Table.Cell>
+                          <span className="text-red-400 hover:underline cursor-pointer">Delete</span>
+                        </Table.Cell>
+                        <Table.Cell>
+                          <NavLink to={`/edit-post/${post._id}`}>Edit</NavLink>
+                        </Table.Cell>
+                      </Table.Row>
+                    </Table.Body>
+                  ))}
+                </Table>
               </div>
-            )
-          }
-             </>
-         ): (
-          <div className="m-auto px-[12px] sm:px-[20px] text-xl "> You have no post yet!</div>
-         )
-        
-        }
-        </div>       
+              {showMore && (
+                <div className="flex justify-center items-center">
+                  <button className="bg-[blue] px-[20px] my-[30px] py-[7px] rounded-lg text-white hover:bg-[gray] cursor-pointer">
+                    Show more
+                  </button>
+                </div>
+              )}
+            </>
+          ) : (
+            <div className="m-auto px-[12px] sm:px-[20px] text-xl">You have no post yet!</div>
+          )}
+        </div>
       </div>
     </>
   );
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,13 +5,19 @@ import App from './App.jsx'
 import { store, persistor } from "./redux/store.js"
 import { Provider } from "react-redux"
 import { PersistGate } from "redux-persist/integration/react"
-// import {QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import {QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
+
   <PersistGate persistor={persistor} >
-  <Provider store={store} >
-   
-    <App />
-    </Provider >,
+    <QueryClientProvider client={queryClient}>
+     <Provider store={store} >
+      <App />
+     </Provider >
+    </QueryClientProvider>
   </ PersistGate >
+
 )
