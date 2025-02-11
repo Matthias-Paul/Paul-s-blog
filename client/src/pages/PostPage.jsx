e11eb7b96d9400280e75c6469a01e8dfc65d5808
Matthias-Paul <140503382+Matthias-Paul@users.noreply.github.com>
2025-02-11 02:22:55 -0800

    update

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -47,26 +47,31 @@ function PostPage() {
 
   return (
     <>
-      <main className="w-full  m-auto px-[12px] sm:px-[20px] pb-[1000px] ">
-        <div className=" h-screen max-w-[1400px] flex flex-col items-center text-center m-auto pt-[100px] ">
-          <div className="  h-screen max-w-[800px] ">
-          <h1 className=" text-[25px] md:text-[40px] px-[30px]    max-w-[600px] font-serif m-auto font-[600] ">{post.title}</h1>
+      <main className="w-full  m-auto px-[12px] sm:px-[20px] pb-[200px]  ">
+        <div className=" max-w-[1400px] flex flex-col items-center text-center m-auto pt-[100px] ">
+          <div className="    ">
+          <h1 className=" text-[25px] md:text-[40px] px-[30px]    max-w-[700px] font-serif m-auto font-[600] ">{post.title}</h1>
           <NavLink to={`/search?category=${post.category}`}>
              <button className=" py-[4px] px-[22px] md:px-[38px] first-letter:uppercase bg-[gray] active:bg-[blue] text-[16px] md:text-[19px] text-white rounded-[12px] my-[12px] " > {post.category} </button>
           </NavLink>
-          <div ><img className=" mt-[20px] object-cover rounded-[5px] max-h-[800px]" src={post.image} alt={post.title} /> </div>
+          <div className="w-full" ><img className=" w-full  mt-[20px] object-cover rounded-[12px] " src={post.image} alt={post.title} /> </div>
           
-          <div className="flex items-center text-[14px] sm:text-[16px] font-[500] ">
-                    <div className="w-[35px] h-[35px]  ">
+          <div className="flex items-center justify-between border-b-[2px] pb-[10px] text-[15px] mt-[7px] md:text-[20px] font-[500] ">
+                   <div className="flex items-center ">
+                    <div className="w-[35px] h-[35px] md:w-[55px] md:h-[55px]   ">
                       <img 
-                        className="w-full h-full rounded-[50%]  "
+                        className="w-full h-full object-cover rounded-[50%]  "
                         src={post.profilePicture}
                         alt="profile"/>
                     </div>
                     <p className=" ml-[7px]  sm:ml-[10px]  ">{post.username}</p>
-                    <p className="  ml-[30px] sm:ml-[40px]">{post.createdAt}</p>
+               </div>
+
+                    <p className=" ">{new Date(post.createdAt).toLocaleDateString()}</p>
                   </div>  
-          <div>{post.content}</div>
+          <div dangerouslySetInnerHTML={{__html:post.content}}  className="text-start font-serif mt-[30px] max-w-[800px] m-auto post-content ">
+
+          </div>
           </div>
         </div>    
       </main>  
